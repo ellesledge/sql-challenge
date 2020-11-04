7c10aa25b4a09b7b4fc43da2cf9b294f2fddbda4
ellesledge <69279540+ellesledge@users.noreply.github.com>
2020-11-03 19:48:14 -0500

    query

--- employee_data.sql
+++ employee_data.sql
@@ -59,3 +59,50 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "department" ("dept_no");
 
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no, 
+		employees.last_name, 
+		employees.first_name, 
+		employees.sex, 
+		salaries.salary
+FROM employees
+INNER JOIN salaries
+ON employees.emp_no = salaries.emp_no;
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employees.first_name,
+		employees.last_name,
+		employees.hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
+
+-- List manager of each depart with: department number, department name, the manager's employee number, last name, first name.
+SELECT department.dept_no,
+		department.dept_name,
+		dept_manager.emp_no,
+		employees.first_name,
+		employees.last_name
+FROM department
+INNER JOIN dept_manager
+ON department.dept_no = dept_manager.dept_no
+INNER JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no,
+		employees.first_name,
+		employees.last_name,
+		department.dept_name
+FROM dept_emp
+INNER JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+INNER JOIN department
+ON dept_emp.dept_no = department.dept_no;
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
